cf0a868a084c7e094fd3f88997e6ea2db78c1471
PrashantSelenium <dhage.prashant@gmail.com>
2016-06-19 20:10:31 +0530

    Added selection helper & its's unit tests

---
+++ Module20/DataDrivenFramework/SelectionHelper.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataDrivenFramework
+{
+    public class SelectionHelper
+    {
+        public static bool IsSuiteSelected(String suiteFilePath, String suiteName)
+        {
+            ExcelReaderFile exl = new ExcelReaderFile(suiteFilePath);
+            int suiteRowNumber = exl.getRowNumber("SuiteList", 0,suiteName);
+            //Console.WriteLine("suiteRowNumber --> " + suiteRowNumber);
+            String suiteRunMode = exl.getCellData("SuiteList",1,suiteRowNumber);
+            //Console.WriteLine("suiteRunMode --> " + suiteRunMode);
+            if (suiteRunMode.Equals("Y",StringComparison.OrdinalIgnoreCase))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+            
+        }
+
+
+        public static bool IsFunctionalitySelected(String suiteFilePath, String funcName)
+        {
+            ExcelReaderFile exl = new ExcelReaderFile(suiteFilePath);
+            int suiteRowNumber = exl.getRowNumber("Functionalties", 0, funcName);
+            String funcRunMode = exl.getCellData("Functionalties", 1, suiteRowNumber);
+            if (funcRunMode.Equals("Y", StringComparison.OrdinalIgnoreCase))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ Module20/TestAutomation/UnitTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using DataDrivenFramework;
+using NUnit.Framework;
+
+namespace TestAutomation
+{
+    [TestFixture]
+    public class UnitTests
+    {
+        [Test]
+        public void SuiteSelectionTest()
+        {
+            Console.WriteLine(SelectionHelper.IsSuiteSelected("H:\\Seleniumc#\\SCDDF\\Module20\\TestAutomation\\TestData\\Suite.xlsx", "SuiteA"));
+            Console.WriteLine(SelectionHelper.IsSuiteSelected("H:\\Seleniumc#\\SCDDF\\Module20\\TestAutomation\\TestData\\Suite.xlsx", "SuiteB"));
+            Console.WriteLine(SelectionHelper.IsSuiteSelected("H:\\Seleniumc#\\SCDDF\\Module20\\TestAutomation\\TestData\\Suite.xlsx", "SuiteC"));
+        }
+
+        
+        [Test]
+        public void FunctionalitySelectionTest()
+        {
+            Console.WriteLine("*******************************************************************************");
+            Console.WriteLine(SelectionHelper.IsFunctionalitySelected("H:\\Seleniumc#\\SCDDF\\Module20\\TestAutomation\\TestData\\SuiteA.xlsx", "Func1"));
+            Console.WriteLine(SelectionHelper.IsFunctionalitySelected("H:\\Seleniumc#\\SCDDF\\Module20\\TestAutomation\\TestData\\SuiteA.xlsx", "Func2"));
+            Console.WriteLine(SelectionHelper.IsFunctionalitySelected("H:\\Seleniumc#\\SCDDF\\Module20\\TestAutomation\\TestData\\SuiteA.xlsx", "Func3"));
+            Console.WriteLine(SelectionHelper.IsFunctionalitySelected("H:\\Seleniumc#\\SCDDF\\Module20\\TestAutomation\\TestData\\SuiteA.xlsx", "Func4"));
+        }
+        
+    }
+}
